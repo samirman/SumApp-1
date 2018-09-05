5391168ef37b33b3c7e3525306b20ba55fa43ead
jconallen <jconallen@us.ibm.com>
2018-09-05 09:46:51 -0400

    reset demo

--- src/main/java/com/ibm/sumapp/Adder.java
+++ src/main/java/com/ibm/sumapp/Adder.java
@@ -5,9 +5,9 @@ public class Adder {
 	
 	public int add( int a, int b ) {
 		int sum = a + b;
-//		if( a == 3 ) {  // introduce an intermittent error
-//			sum = a * b;
-//		} 
+		if( a == 3 ) {  // introduce an intermittent error
+			sum = a * b;
+		} 
 		return sum;
 	}
 	
